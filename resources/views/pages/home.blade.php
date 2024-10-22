196d12bc50f0b5f5e465837b37ce38c445da830c
AngeloCrisci <angelocrisci1998@gmail.com>
2024-10-22 16:52:14 +0200

    FIxed scss

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -11,6 +11,8 @@
             <div class="card-body">
             <h1 class="card-text">Titolo: {{ $comic["title"] }}</h1>
             <p class="card-text">Descrizione: {{ $comic["description"] }}</p>
+            <p class="card-text"><strong> Prezzo: {{ $comic["price"] }} </strong></p>
+            <p class="card-text"><strong> Serie: {{ $comic["series"] }}</strong></p>
             </div>
         </div>
     @endforeach
